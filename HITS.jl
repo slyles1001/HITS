26d35aeb6a7a02b8b0dbb70eba24c2beb89bff5d
Seth Bromberger <sbromberger@users.noreply.github.com>
2018-05-25 09:04:41 -0700

    Update HITS.jl

--- HITS.jl
+++ HITS.jl
@@ -9,12 +9,12 @@ function HITS(g::AbstractGraph, mx=100, tol=1e-6)
     a0 = fill(0, N)
     h0 = fill(1. / N, N)
     ag = adjacency_matrix(g)
+    agT = is_directed(g) ? ag' : ag
     for _ in 1:mx
-        auth = normalize(ag' * h0)
+        auth = normalize(agT * h0)
         hubs = normalize(ag * auth)
         sum(abs.(hubs - h0)) <= tol && return (hubs, auth)
     end
     warn("Did not converge")
     return (hubs, auth)
 end
-		
